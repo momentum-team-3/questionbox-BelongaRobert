e157467964a5fa1fdd1c1b8e95f0db1c13ed2c9e
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-09-08 20:38:30 -0400

    Adding some stuff

--- project/urls.py
+++ project/urls.py
@@ -25,6 +25,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
+        path
 
         # For django versions before 2.0:
         # url(r'^__debug__/', include(debug_toolbar.urls)),
