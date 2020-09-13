b7d17ee84898f7f15130928b7aa96cacf937a961
BelongaRobert <68036456+BelongaRobert@users.noreply.github.com>
2020-09-13 10:19:33 -0400

    latest

--- questionbox/questions/views.py
+++ questionbox/questions/views.py
@@ -1,13 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
 from django.views.generic.detail import DetailView
 from Django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from .models import Question, Answer
-from django.views import View
+from django.views.generic import View
 
 
 class list_question(ListView):
     model = Question
     template_name = "list_question.html"
+    
 
 class view_question(DetailView):
     model = Question
